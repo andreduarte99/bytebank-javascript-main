46361e69f96c5fc020906914e8bb867514fe12fa
andreduarte99 <andre.duarte16@outlook.com>
2025-04-19 11:52:07 -0300

    finalizando projeto

--- script/scripts.js
+++ script/scripts.js
@@ -1,4 +1,4 @@
-import imprimeCotacao from "./imprimeCotacao.js";
+import selecionaCotacao from "./imprimeCotacao.js";
 const graficoDolar = document.getElementById("graficoDolar");
 
 const graficoParaDolar = new Chart(graficoDolar, {
@@ -37,6 +37,55 @@ workerDolar.postMessage("usd");
 workerDolar.addEventListener("message", (event) => {
   let tempo = geraHorario();
   let valor = event.data.ask;
-  imprimeCotacao("dolar", valor);
+  selecionaCotacao("dolar", valor);
   adicionarDados(graficoParaDolar, tempo, valor);
 });
+
+const graficoIene = document.getElementById("graficoIene");
+const graficoParaIene = new Chart(graficoIene, {
+  type: 'line',
+  data:{
+    labels: [],
+    datasets: [
+      {
+        label: "Iene",
+        data: [],
+        borderWidth: 1,
+      },
+    ],
+  }
+});
+
+let workerIene = new Worker("./script/workers/workerIene.js");
+workerIene.postMessage("iene");
+workerIene.addEventListener("message", (event) => {
+  let tempo = geraHorario();
+  let valor = event.data.ask;
+  selecionaCotacao("iene", valor);
+  adicionarDados(graficoParaIene, tempo, valor);
+});
+
+const graficoEuro = document.getElementById("graficoEuro");
+const graficoParaEuro = new Chart(graficoEuro, {
+  type: 'line',
+  data:{
+    labels: [],
+    datasets: [
+      {
+        label: "Euro",
+        data: [],
+        borderWidth: 1,
+      },
+    ],
+  }
+});
+
+let workerEuro = new Worker("./script/workers/workerEuro.js");
+workerEuro.postMessage("euro");
+workerEuro.addEventListener("message", (event) => {
+  let tempo = geraHorario();
+  let valor = event.data.ask;
+  selecionaCotacao("euro", valor);
+  adicionarDados(graficoParaEuro, tempo, valor);
+});
+
